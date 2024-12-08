4483923baadaa6b4bc46cb41048de805e5e88ab4
Morgan White <morgancjwhite@gmail.com>
2024-12-08 12:38:21 +0000

    Complete 3.1

---
+++ 3/3.1.ex
@@ -0,0 +1,18 @@
+defmodule DataParser do
+  def parse_file(filename) do
+    File.stream!(filename)
+    |> Enum.map(&String.trim/1)
+    |> Enum.join()
+  end
+end
+
+data = DataParser.parse_file("3.input")
+
+sum =
+  # Look for mul(NUM,NUM) pattern an capture both numbers
+  Regex.scan(~r/mul\((?<x>[0-9]+),(?<y>[0-9]+)\)/, data)
+  # Unpack matches and multiply
+  |> Enum.map(fn [_, x, y] -> String.to_integer(x) * String.to_integer(y) end)
+  |> Enum.sum()
+
+IO.inspect(sum)
